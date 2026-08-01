e2703a1a2e3e7155ffdff395d4fd9879d63cfb07
newwonwoo <maspiece83@gmail.com>
2026-08-01 16:40:47 +0900

    Fix WorkManager progress observation

---
+++ dama/app/src/main/java/androidx/work/WorkManagerFlow.kt
@@ -0,0 +1,19 @@
+package androidx.work
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
+import java.util.UUID
+
+fun WorkManager.getWorkInfoByIdFlow(id: UUID): Flow<WorkInfo> = flow {
+    while (true) {
+        val info = withContext(Dispatchers.IO) {
+            getWorkInfoById(id).get()
+        }
+        emit(info)
+        if (info.state.isFinished) break
+        delay(500)
+    }
+}
